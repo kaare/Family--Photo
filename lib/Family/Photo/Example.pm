dc584887c83151d10e363d8ea2495c264fde412a
Kaare Rasmussen <kaare@jasonic.dk>
2011-05-09 12:47:58 +0200

    Test multiple file upload in Mojo

--- lib/Family/Photo/Example.pm
+++ lib/Family/Photo/Example.pm
@@ -1,12 +1,18 @@
 package Family::Photo::Example;
+use 5.010;
 use Mojo::Base 'Mojolicious::Controller';
 
 # This action will render a template
 sub welcome {
-  my $self = shift;
+	my $self = shift;
 
-  # Render template "example/welcome.html.ep" with message
-  $self->render(message => 'Welcome to the Mojolicious Web Framework!');
+	say STDERR ref $self->req->uploads;
+
+	for my $upload (@{ $self->req->uploads }) {
+		say STDERR $upload->filename if $upload;
+	}
+	# Render template "example/welcome.html.ep" with message
+	$self->render(message => 'Welcome to the Mojolicious Web Framework!');
 }
 
 1;
